eec20625b9a48f0a24469a371d64e957024de1b1
hujiaju <nanhujiaju@gmail.com>
2018-04-13 10:05:25 +0800

    add base class for model

---
+++ MultiEducation/Models/MEM.h
@@ -0,0 +1,16 @@
+//
+//  MEM.h
+//  MultiEducation
+//
+//  Created by nanhu on 2018/4/13.
+//  Copyright © 2018年 niuduo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/**
+ Model base class
+ */
+@interface MEM : NSObject
+
+@end
---
+++ MultiEducation/Models/MEM.m
@@ -0,0 +1,13 @@
+//
+//  MEM.m
+//  MultiEducation
+//
+//  Created by nanhu on 2018/4/13.
+//  Copyright © 2018年 niuduo. All rights reserved.
+//
+
+#import "MEM.h"
+
+@implementation MEM
+
+@end
