7e5fde0f453b16919baebf55b4ce1e5044a711ff
HiddenKill <787779444@qq.com>
2018-04-16 13:11:14 +0800

    upload image cell

---
+++ MultiEducation/UIComponents/babyContent/babyContentCell/MEProgressCell.m
@@ -0,0 +1,24 @@
+//
+//  MEProgressCell.m
+//  MultiEducation
+//
+//  Created by 崔小舟 on 2018/4/16.
+//  Copyright © 2018年 niuduo. All rights reserved.
+//
+
+#import "MEProgressCell.h"
+
+@implementation MEProgressCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
