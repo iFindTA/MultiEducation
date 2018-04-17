5639efa2bd3cf10bd738c1eeced3c332c5f13460
hujiaju <nanhujiaju@gmail.com>
2018-04-17 16:17:43 +0800

    local

---
+++ MultiEducation/UIComponents/indexHeader/MEIndexNavigationBar.h
@@ -0,0 +1,13 @@
+//
+//  MEIndexNavigationBar.h
+//  MultiEducation
+//
+//  Created by nanhu on 2018/4/17.
+//  Copyright © 2018年 niuduo. All rights reserved.
+//
+
+#import "MEBaseScene.h"
+
+@interface MEIndexNavigationBar : MEBaseScene
+
+@end
