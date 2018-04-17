0f1a796f23194d610a84f464a311030d6c9dc5ef
hujiaju <nanhujiaju@gmail.com>
2018-04-17 10:00:24 +0800

    create tcp service class

---
+++ MultiEducation/NetService/METCPService.m
@@ -0,0 +1,13 @@
+//
+//  METCPService.m
+//  MultiEducation
+//
+//  Created by nanhu on 2018/4/17.
+//  Copyright © 2018年 niuduo. All rights reserved.
+//
+
+#import "METCPService.h"
+
+@implementation METCPService
+
+@end
