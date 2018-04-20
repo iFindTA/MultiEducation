a67425e7251d4b63343594140e070a9c3f807215
hehang <leehang@126.com>
2018-04-20 18:04:47 +0800

    聊天列表,聊天页~

---
+++ MultiEducation/Profiles/chat/MEChatProfile.h
@@ -0,0 +1,15 @@
+//
+//  MEChatProfile.h
+//  MultiEducation
+//
+//  Created by mac on 2018/4/20.
+//  Copyright © 2018年 niuduo. All rights reserved.
+//
+
+#import <RongIMKit/RongIMKit.h>
+
+@interface MEChatProfile : RCConversationViewController
+
+
+
+@end
