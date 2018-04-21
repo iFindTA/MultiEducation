4482c19cb91c39c25b9a3c28255c52c1154d81c5
hujiaju <nanhujiaju@gmail.com>
2018-04-21 10:44:11 +0800

    add new file for live mask

---
+++ MultiEducation/UIComponents/liveClass/MELiveMaskLayer.m
@@ -0,0 +1,21 @@
+//
+//  MELiveMaskLayer.m
+//  MultiEducation
+//
+//  Created by nanhu on 2018/4/21.
+//  Copyright © 2018年 niuduo. All rights reserved.
+//
+
+#import "MELiveMaskLayer.h"
+
+@implementation MELiveMaskLayer
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
