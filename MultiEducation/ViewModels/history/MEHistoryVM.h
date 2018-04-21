42c5385e00f2b076a7db7fdf03c2bb6f05eeb8ad
hujiaju <nanhujiaju@gmail.com>
2018-04-21 16:10:05 +0800

    add history and collection

---
+++ MultiEducation/ViewModels/history/MEHistoryVM.h
@@ -0,0 +1,13 @@
+//
+//  MEHistoryVM.h
+//  MultiEducation
+//
+//  Created by nanhu on 2018/4/21.
+//  Copyright © 2018年 niuduo. All rights reserved.
+//
+
+#import "MEVM.h"
+
+@interface MEHistoryVM : MEVM
+
+@end
