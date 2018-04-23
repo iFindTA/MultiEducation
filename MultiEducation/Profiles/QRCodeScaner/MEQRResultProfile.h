c84d356b827292d274daa1f48d51718a1bd3563a
hujiaju <nanhujiaju@gmail.com>
2018-04-23 20:23:14 +0800

    new profile for qr code

---
+++ MultiEducation/Profiles/QRCodeScaner/MEQRResultProfile.h
@@ -0,0 +1,13 @@
+//
+//  MEQRResultProfile.h
+//  MultiEducation
+//
+//  Created by nanhu on 2018/4/23.
+//  Copyright © 2018年 niuduo. All rights reserved.
+//
+
+#import "MEBaseProfile.h"
+
+@interface MEQRResultProfile : MEBaseProfile
+
+@end
---
+++ MultiEducation/Profiles/QRCodeScaner/MEQRScanProfile.h
@@ -0,0 +1,13 @@
+//
+//  MEQRScanProfile.h
+//  MultiEducation
+//
+//  Created by nanhu on 2018/4/23.
+//  Copyright © 2018年 niuduo. All rights reserved.
+//
+
+#import "MEBaseProfile.h"
+
+@interface MEQRScanProfile : MEBaseProfile
+
+@end
