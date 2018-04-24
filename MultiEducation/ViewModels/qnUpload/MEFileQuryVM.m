b83cf3471d5848cbeaece792d983d092408b6e90
hujiaju <nanhujiaju@gmail.com>
2018-04-24 14:14:39 +0800

    add vm for qnfile

---
+++ MultiEducation/ViewModels/qnUpload/MEFileQuryVM.m
@@ -0,0 +1,13 @@
+//
+//  MEFileQuryVM.m
+//  MultiEducation
+//
+//  Created by nanhu on 2018/4/24.
+//  Copyright © 2018年 niuduo. All rights reserved.
+//
+
+#import "MEFileQuryVM.h"
+
+@implementation MEFileQuryVM
+
+@end
