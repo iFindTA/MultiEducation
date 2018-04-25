a5fcd4dd4db3ed327c980677eac83d85a4cf4f69
hujiaju <nanhujiaju@gmail.com>
2018-04-25 09:59:24 +0800

    add new vm for class member

---
+++ MultiEducation/ViewModels/chat/MEClassMemberVM.h
@@ -0,0 +1,13 @@
+//
+//  MEClassMemberVM.h
+//  MultiEducation
+//
+//  Created by nanhu on 2018/4/24.
+//  Copyright © 2018年 niuduo. All rights reserved.
+//
+
+#import "MEVM.h"
+
+@interface MEClassMemberVM : MEVM
+
+@end
---
+++ MultiEducation/ViewModels/chat/MEClassMemberVM.m
@@ -0,0 +1,13 @@
+//
+//  MEClassMemberVM.m
+//  MultiEducation
+//
+//  Created by nanhu on 2018/4/24.
+//  Copyright © 2018年 niuduo. All rights reserved.
+//
+
+#import "MEClassMemberVM.h"
+
+@implementation MEClassMemberVM
+
+@end
