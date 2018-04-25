3f161dac011931b0807d2f97db862e931ccb84bd
hujiaju <nanhujiaju@gmail.com>
2018-04-25 11:44:29 +0800

    add contact detail profile

---
+++ MultiEducation/Profiles/chatContact/MEContactInfoProfile.h
@@ -0,0 +1,13 @@
+//
+//  MEContactInfoProfile.h
+//  MultiEducation
+//
+//  Created by nanhu on 2018/4/25.
+//  Copyright © 2018年 niuduo. All rights reserved.
+//
+
+#import "MEBaseProfile.h"
+
+@interface MEContactInfoProfile : MEBaseProfile
+
+@end
