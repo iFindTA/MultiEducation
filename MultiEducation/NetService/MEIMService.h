16af7e1f8de1d3ffede2af86554de9d4ac658a23
hujiaju <nanhujiaju@gmail.com>
2018-04-25 19:36:55 +0800

    add newf files

---
+++ MultiEducation/NetService/MEIMService.h
@@ -0,0 +1,13 @@
+//
+//  MEIMService.h
+//  MultiEducation
+//
+//  Created by nanhu on 2018/4/25.
+//  Copyright © 2018年 niuduo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MEIMService : NSObject
+
+@end
---
+++ MultiEducation/NetService/MEIMService.m
@@ -0,0 +1,13 @@
+//
+//  MEIMService.m
+//  MultiEducation
+//
+//  Created by nanhu on 2018/4/25.
+//  Copyright © 2018年 niuduo. All rights reserved.
+//
+
+#import "MEIMService.h"
+
+@implementation MEIMService
+
+@end
