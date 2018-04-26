0987275fff73a8058827d68b5e8e4b86ad640dba
HiddenKill <787779444@qq.com>
2018-04-26 09:22:54 +0800

    for pull

---
+++ MultiEducation/Profiles/baby/MESelectFolderProfile.h
@@ -0,0 +1,13 @@
+//
+//  MESelectFolderProfile.h
+//  MultiEducation
+//
+//  Created by 崔小舟 on 2018/4/26.
+//  Copyright © 2018年 niuduo. All rights reserved.
+//
+
+#import "MEBaseProfile.h"
+
+@interface MESelectFolderProfile : MEBaseProfile
+
+@end
---
+++ MultiEducation/Profiles/baby/MESelectFolderProfile.m
@@ -0,0 +1,38 @@
+//
+//  MESelectFolderProfile.m
+//  MultiEducation
+//
+//  Created by 崔小舟 on 2018/4/26.
+//  Copyright © 2018年 niuduo. All rights reserved.
+//
+
+#import "MESelectFolderProfile.h"
+
+@interface MESelectFolderProfile ()
+
+@end
+
+@implementation MESelectFolderProfile
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+- (void)customNavigation {
+    NSString *title = @"移动到";
+    UIBarButtonItem *spacer = [self barSpacer];
+    UIBarButtonItem *backItem = [self backBarButtonItem:nil withIconUnicode:@"\U0000e6e2"];
+    UINavigationItem *navigationItem = [[UINavigationItem alloc] initWithTitle:title];
+    navigationItem.leftBarButtonItems = @[spacer, backItem];
+    [self.navigationBar pushNavigationItem: navigationItem animated:true];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+
+}
+
+
+
+@end
