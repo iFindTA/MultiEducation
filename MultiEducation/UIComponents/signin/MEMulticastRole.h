adc5c4f37a7fc787185d8805ef4b34c4f18d0efa
hujiaju <nanhujiaju@gmail.com>
2018-05-07 20:04:53 +0800

    delete old files

---
+++ MultiEducation/UIComponents/signin/MEMulticastRole.h
@@ -0,0 +1,13 @@
+//
+//  MEMulticastRole.h
+//  MultiEducation
+//
+//  Created by nanhu on 2018/5/7.
+//  Copyright © 2018年 niuduo. All rights reserved.
+//
+
+#import "MEBaseScene.h"
+
+@interface MEMulticastRole : MEBaseScene
+
+@end
---
+++ MultiEducation/UIComponents/signin/MEMulticastRole.m
@@ -0,0 +1,21 @@
+//
+//  MEMulticastRole.m
+//  MultiEducation
+//
+//  Created by nanhu on 2018/5/7.
+//  Copyright © 2018年 niuduo. All rights reserved.
+//
+
+#import "MEMulticastRole.h"
+
+@implementation MEMulticastRole
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
