935c4e5581832deacd499e7a087216e87d6d45a7
hujiaju <nanhujiaju@gmail.com>
2018-05-16 15:03:11 +0800

    add buriad for umeng

---
+++ MultiEducation/Macros/StatisticalAnalysis.h
@@ -0,0 +1,13 @@
+//
+//  StatisticalAnalysis.h
+//  MultiEducation
+//
+//  Created by nanhu on 2018/5/16.
+//  Copyright © 2018年 niuduo. All rights reserved.
+//
+
+#ifndef StatisticalAnalysis_h
+#define StatisticalAnalysis_h
+
+
+#endif /* StatisticalAnalysis_h */
