6298a83c0525f81209d84dfc9bb268ba3c143f39
hujiaju <nanhujiaju@gmail.com>
2018-05-18 09:24:51 +0800

    new files

---
+++ MultiEducation/Profiles/GrowthEvaluate/MEGrowthEvaRootProfile.m
@@ -0,0 +1,37 @@
+//
+//  MEGrowthEvaRootProfile.m
+//  MultiEducation
+//
+//  Created by nanhu on 2018/5/18.
+//  Copyright © 2018年 niuduo. All rights reserved.
+//
+
+#import "MEGrowthEvaRootProfile.h"
+
+@interface MEGrowthEvaRootProfile ()
+
+@end
+
+@implementation MEGrowthEvaRootProfile
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ MultiEducation/Profiles/SemesterEvaluate/MESemesterEvaRootProfile.h
@@ -0,0 +1,13 @@
+//
+//  MESemesterEvaRootProfile.h
+//  MultiEducation
+//
+//  Created by nanhu on 2018/5/18.
+//  Copyright © 2018年 niuduo. All rights reserved.
+//
+
+#import "MEBaseProfile.h"
+
+@interface MESemesterEvaRootProfile : MEBaseProfile
+
+@end
