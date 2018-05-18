cae732869cd0648e96ed33de9841cc5be0a56fe9
hujiaju <nanhujiaju@gmail.com>
2018-05-18 20:49:50 +0800

    new vm file

---
+++ MultiEducation/ViewModels/MEStudentInfoVM.h
@@ -0,0 +1,13 @@
+//
+//  MEStudentInfoVM.h
+//  MultiEducation
+//
+//  Created by nanhu on 2018/5/18.
+//  Copyright © 2018年 niuduo. All rights reserved.
+//
+
+#import "MEVM.h"
+
+@interface MEStudentInfoVM : MEVM
+
+@end
