31c1d129893bd9aac3ecd0ec527181905235f0b1
hujiaju <nanhujiaju@gmail.com>
2018-05-18 13:53:56 +0800

    add new files

---
+++ MultiEducation/UIComponents/studentPanel/MEStudentsPanel.h
@@ -0,0 +1,13 @@
+//
+//  MEStudentsPanel.h
+//  MultiEducation
+//
+//  Created by nanhu on 2018/5/18.
+//  Copyright © 2018年 niuduo. All rights reserved.
+//
+
+#import "MEBaseScene.h"
+
+@interface MEStudentsPanel : MEBaseScene
+
+@end
---
+++ MultiEducation/UIComponents/studentPanel/MEStudentsPanel.m
@@ -0,0 +1,21 @@
+//
+//  MEStudentsPanel.m
+//  MultiEducation
+//
+//  Created by nanhu on 2018/5/18.
+//  Copyright © 2018年 niuduo. All rights reserved.
+//
+
+#import "MEStudentsPanel.h"
+
+@implementation MEStudentsPanel
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
