1d70301f38efb7b9ef0cdf262634433445375381
hujiaju <nanhujiaju@gmail.com>
2018-05-18 15:13:10 +0800

    new vm

---
+++ MultiEducation/ViewModels/MEStudentListVM.h
@@ -0,0 +1,13 @@
+//
+//  MEStudentListVM.h
+//  MultiEducation
+//
+//  Created by nanhu on 2018/5/18.
+//  Copyright © 2018年 niuduo. All rights reserved.
+//
+
+#import "MEVM.h"
+
+@interface MEStudentListVM : MEVM
+
+@end
---
+++ MultiEducation/ViewModels/MEStudentListVM.m
@@ -0,0 +1,13 @@
+//
+//  MEStudentListVM.m
+//  MultiEducation
+//
+//  Created by nanhu on 2018/5/18.
+//  Copyright © 2018年 niuduo. All rights reserved.
+//
+
+#import "MEStudentListVM.h"
+
+@implementation MEStudentListVM
+
+@end
