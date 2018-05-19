6af04b638b2539634420687bb0616158f67d0ca6
hujiaju <nanhujiaju@gmail.com>
2018-05-19 11:08:50 +0800

    new files

---
+++ MultiEducation/UIComponents/evaluatePanel/MESubNavigator.h
@@ -0,0 +1,13 @@
+//
+//  MESubNavigator.h
+//  MultiEducation
+//
+//  Created by nanhu on 2018/5/19.
+//  Copyright © 2018年 niuduo. All rights reserved.
+//
+
+#import "MEBaseScene.h"
+
+@interface MESubNavigator : MEBaseScene
+
+@end
---
+++ MultiEducation/UIComponents/evaluatePanel/MESubNavigator.m
@@ -0,0 +1,21 @@
+//
+//  MESubNavigator.m
+//  MultiEducation
+//
+//  Created by nanhu on 2018/5/19.
+//  Copyright © 2018年 niuduo. All rights reserved.
+//
+
+#import "MESubNavigator.h"
+
+@implementation MESubNavigator
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
