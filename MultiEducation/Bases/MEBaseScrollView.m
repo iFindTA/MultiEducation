e5a14f12ceb20240764ee40b2850528cd5399f84
hujiaju <nanhujiaju@gmail.com>
2018-05-19 17:34:45 +0800

    new scrollview

---
+++ MultiEducation/Bases/MEBaseScrollView.m
@@ -0,0 +1,21 @@
+//
+//  MEBaseScrollView.m
+//  MultiEducation
+//
+//  Created by nanhu on 2018/5/19.
+//  Copyright © 2018年 niuduo. All rights reserved.
+//
+
+#import "MEBaseScrollView.h"
+
+@implementation MEBaseScrollView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
