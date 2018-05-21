bbbab25f5128bec10c947589e1cf3072fe5e7642
hujiaju <nanhujiaju@gmail.com>
2018-05-21 16:52:01 +0800

    new files

---
+++ MultiEducation/Profiles/GrowthEvaluate/MEForwardGEProfile.h
@@ -0,0 +1,13 @@
+//
+//  MEForwardGEProfile.h
+//  MultiEducation
+//
+//  Created by nanhu on 2018/5/21.
+//  Copyright © 2018年 niuduo. All rights reserved.
+//
+
+#import "MEBaseProfile.h"
+
+@interface MEForwardGEProfile : MEBaseProfile
+
+@end
