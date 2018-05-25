287ba47ff1b57806ce9d8e76b2213d91e380ad5e
HiddenKill <787779444@qq.com>
2018-05-25 14:05:45 +0800

    pull for pod

--- MultiEducation/UIComponents/babyContent/babyInteresting/babySendInterseting/MEBabyIntersetingSelectView.h
+++ MultiEducation/UIComponents/babyContent/babyInteresting/babySendInterseting/MEBabyIntersetingSelectView.h
@@ -7,15 +7,18 @@
 //
 
 #import "MEBaseScene.h"
-@class MEStudentModel;
+#import "MEStudentModel.h"
 
 @interface MEBabyIntersetingSelectView : MEBaseScene
 
+@property (nonatomic, copy) void(^didSelectStuCallback) (NSArray *stuArr);
 @property (nonatomic, copy) void(^DidRemakeMasonry) (UIView *bottomView);
 
 @property (nonatomic, assign) int64_t semester;
 @property (nonatomic, assign) int64_t classId;
 @property (nonatomic, assign) int64_t gradeId;
+
+@property (nonatomic, strong) NSMutableArray <MEStudentModel *> *dataArr;
 
 - (void)customSubviews;
 
--- MultiEducation/UIComponents/babyContent/babyInteresting/babySendInterseting/MESendIntersetContent.h
+++ MultiEducation/UIComponents/babyContent/babyInteresting/babySendInterseting/MESendIntersetContent.h
@@ -9,6 +9,7 @@
 #import "MEBaseScene.h"
 @class MEBabyIntersetingSelectView;
 @class MEStudentModel;
+@class MEStuInterestModel;
 
 @interface MESendIntersetContent : MEBaseScene
 
@@ -20,6 +21,8 @@
 
 @property (nonatomic, strong) NSMutableArray *selectedImages;   //当前选中的image
 @property (nonatomic, copy) void(^DidPickerButtonTouchCallback) (void);
+@property (nonatomic, copy) void(^didFetchlocalizationDataHandler) (MEStuInterestModel *model);
+
 
 - (void)didSelectImagesOrVideo:(NSArray <NSDictionary *> *)images;
 
@@ -31,5 +34,11 @@
 - (NSString *)getInterestTitle;
 //获取趣事趣影内容
 - (NSString *)getInterestContext;
+
+
+/**
+ 根据是否有缓存数据决定是否显示alert
+ */
+- (void)showAlert2UserWhetherFetchStuInterestModel;
 
 @end
