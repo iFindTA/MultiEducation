0852ad59ffdfe22b1d95218ae893f4322caf7d29
hujiaju <nanhujiaju@gmail.com>
2018-06-01 14:42:10 +0800

    add baby archive profile

---
+++ MultiEducation/Profiles/BabyArchives/MEBabyArchiveProfile.h
@@ -0,0 +1,13 @@
+//
+//  MEBabyArchiveProfile.h
+//  MultiEducation
+//
+//  Created by nanhu on 2018/6/1.
+//  Copyright © 2018年 niuduo. All rights reserved.
+//
+
+#import "MEBaseProfile.h"
+
+@interface MEBabyArchiveProfile : MEBaseProfile
+
+@end
