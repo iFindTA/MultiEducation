7bed1755da07afd407a750aeed6a5d7b6c090d5a
hujiaju <nanhujiaju@gmail.com>
2018-07-09 16:15:16 +0800

    add pradient layer fot index

---
+++ MultiEducation/UIComponents/indexContent/MEIndexBgScroller/MEGradientLayer.h
@@ -0,0 +1,13 @@
+//
+//  MEGradientLayer.h
+//  MultiEducation
+//
+//  Created by nanhu on 2018/7/9.
+//  Copyright © 2018年 niuduo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MEGradientLayer : UIView
+
+@end
---
+++ MultiEducation/UIComponents/indexContent/MEIndexBgScroller/MEGradientLayer.m
@@ -0,0 +1,21 @@
+//
+//  MEGradientLayer.m
+//  MultiEducation
+//
+//  Created by nanhu on 2018/7/9.
+//  Copyright © 2018年 niuduo. All rights reserved.
+//
+
+#import "MEGradientLayer.h"
+
+@implementation MEGradientLayer
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
